f0f48dc772cd319343a4ab84a2ac8660e79485b8
kaya <kaaya.xx.x@gmail.com>
2023-10-26 11:50:31 +0900

    タスクの新規作成・編集・詳細機能の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,8 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[ show edit destroy update ]
+
   def index
+    @tasks = Task.all
   end
 
   def show
@@ -8,13 +11,37 @@ class TasksController < ApplicationController
   def edit
   end
 
+  def update
+    if @task.update(task_params)
+      redirect_to task_path, notice: "タスクを編集しました"
+    else
+      render :edit
+    end
+  end
+
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスクを作成しました"
+    else
+      render :new
+    end
   end
 
   def destroy
   end
 
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
 end
