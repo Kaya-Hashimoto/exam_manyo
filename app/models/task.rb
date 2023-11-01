17c6f4063ca1a92a05c02a18de6bf42748b67254
kaya <kaaya.xx.x@gmail.com>
2023-11-01 23:35:49 +0900

    ステータスを日本語表記に変更

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,7 @@ class Task < ApplicationRecord
   has_many :labelings, dependent: :destroy
   validates :title, presence: true
   validates :content, presence: true, length: { maximum: 250}
-  enum status: { status_not_started: 0, status_start: 1, status_complete: 2 }
+  enum status: { "未着手": 0, "着手中": 1, "完了": 2 }
+
+  
 end
