cb0126c1965676af7e3c9c3577a34085d3d6eddc
kaya <kaaya.xx.x@gmail.com>
2023-11-08 18:22:12 +0900

    コールバックの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  before_destroy :admin_cannot_delete
+  before_update :admin_cannot_update
+
   has_many :tasks, dependent: :destroy
   validates :name, presence: true, length: {maximum: 15}
   validates :email, presence: true, length: {maximum: 255},
@@ -6,5 +9,20 @@ class User < ApplicationRecord
   before_validation { email.downcase! }
   has_secure_password
   validates :password, length: { minimum: 6 }
-  
+
+  private
+
+  def admin_cannot_update
+    if User.where(admin: true).count == 1 && self.admin == false
+      errors.add(:base, '管理者は必ず一人必要です')
+      throw :abort
+    end
+  end
+
+  def admin_cannot_delete
+    if User.where(admin: true).count == 1 && self.admin == true
+      errors.add(:base, '管理者は必ず一人必要です')
+      throw :abort
+    end
+  end
 end
